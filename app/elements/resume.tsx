e0e01b3751c961a992c50b43421ad4dab7d97e79
anujkarki <68675633+anujkarki@users.noreply.github.com>
2024-04-01 17:45:05 +0545

    Resume updated

--- app/elements/resume.tsx
+++ app/elements/resume.tsx
@@ -1,125 +1,147 @@
-// components/ResumeSite.js
-
 import React from 'react';
+import { EmailIcon, GithubIcon, LocationIcon, PhoneIcon } from './svg';
+import Image from "next/image";
 
-const ResumeSite = () => {
-  const phoneNumber = '123-456-7890';
-  const emailAddress = 'example@example.com';
+const skills = [
+  "JavaScript (ES6+)",
+  "React.js",
+  "Node.js",
+  ".NET Framework (C#)",
+  "HTML5",
+  "CSS3",
+  "SQL",
+  "MS SQL Server",
+  "Git",
+];
 
-  const handleCall = () => {
-    window.location.href = `tel:${phoneNumber.replace(/-/g, '')}`;
-  };
-
-  const handleEmail = () => {
-    window.location.href = `mailto:${emailAddress}`;
-  };
-
+const Resume = () => {
   return (
-    <div className="max-w-3xl mx-auto p-4">
-      <header className="mb-8">
-        <h1 className="text-3xl font-bold mb-2">Your Name</h1>
-        <p className="text-gray-600">Your Position</p>
-        <div className="flex items-center mt-4">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5 mr-2 text-gray-500 cursor-pointer"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            onClick={handleCall}
-          >
-            <path
-              fillRule="evenodd"
-              d="M10 2a8 8 0 100 16 8 8 0 000-16zM0 10a10 10 0 012-6.33V4a1 1 0 011-1h2.67a1 1 0 01.7.29l8.77 8.78a1 1 0 01.23.33l1.72 5.16a1 1 0 01-.33 1.1A10 10 0 010 10zm4.72-4.72a8 8 0 0111.56 11.56l-1.75-5.25a1 1 0 01.16-.78l5.29-5.3a8 8 0 01-11.56 11.56l-4.24-4.24a8 8 0 010-11.32zm9.9 9.9l-3.25-3.25a1 1 0 00-1.41 1.41l3.25 3.25a1 1 0 001.41-1.41z"
-              clipRule="evenodd"
-            />
-          </svg>
-          <span className="mr-2" onClick={handleCall}>{phoneNumber}</span>
-        </div>
-        <div className="flex items-center mt-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5 mr-2 text-gray-500 cursor-pointer"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            onClick={handleEmail}
-          >
-            <path
-              fillRule="evenodd"
-              d="M4 2a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V4a2 2 0 00-2-2H4zM3 4a1 1 0 011-1h12a1 1 0 011 1v12a1 1 0 01-1 1H4a1 1 0 01-1-1V4z"
-              clipRule="evenodd"
-            />
-            <path d="M8 11a1 1 0 100-2 1 1 0 000 2zM8 13a1 1 0 100-2 1 1 0 000 2z" />
-          </svg>
-          <span className="mr-2" onClick={handleEmail}>{emailAddress}</span>
-        </div>
-      </header>
-      <section className="mb-8">
-        <h2 className="text-xl font-bold mb-2 border-b-2 pb-2">About Me</h2>
-        <p className="text-gray-800">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla varius libero eu odio facilisis, sit amet blandit odio interdum.</p>
-      </section>
-      <section className="mb-8">
-        <h2 className="text-xl font-bold mb-2 border-b-2 pb-2">Experience</h2>
-        <div>
-          <div className="mb-4">
-            <h3 className="text-lg font-bold">Company A</h3>
-            <p><em>Address A</em></p>
-            <p><em>Position A | Duration A</em></p>
-            <ul className="list-disc list-inside">
-              <li>Responsibility A1</li>
-              <li>Responsibility A2</li>
-            </ul>
+    <div className="bg-gray-100 min-h-screen py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-4xl mx-auto bg-white shadow overflow-hidden sm:rounded-lg">
+        {/* Header */}
+        <div className="px-4 py-5 sm:px-6">
+          <h3 className="text-3xl leading-8 font-bold text-gray-900">Anuj Karki</h3>
+          <p className="mt-1 max-w-2xl text-lg text-gray-500">Software Developer</p>
+          {/* Contact Details */}
+          <div className="flex mt-2">
+            <div className="mr-4">
+              <span className="text-lg text-gray-500">
+                <EmailIcon />
+                </span>
+              <a href="mailto:anujkarki553@gmail.com" className="ml-2 text-lg text-gray-900">anujkarki553@gmail.com</a>
+            </div>
+            <div className="mr-4">
+              <span className="text-lg text-gray-500"><PhoneIcon/>
+              </span>
+              
+              <a href="tel:+9779806030587" className="ml-2 text-lg text-gray-900">+977 9806030587</a>
+            </div>
+            <div className="mr-4">
+              <span className="text-lg text-gray-500"><LocationIcon/></span>
+              
+              <a href="#" className="ml-2 text-lg text-gray-900">Gatthaghar, Bhaktapur</a>
+            </div>
           </div>
-          <div className="mb-4">
-            <h3 className="text-lg font-bold">Company B</h3>
-            <p><em>Address B</em></p>
-            <p><em>Position B | Duration B</em></p>
-            <ul className="list-disc list-inside">
-              <li>Responsibility B1</li>
-              <li>Responsibility B2</li>
-            </ul>
+          {/* Social Media Links */}
+          <div className="flex mt-2 hidden">
+            <a href="https://linkedin.com/johndoe" className="mr-4 text-blue-500 hover:underline">LinkedIn</a>
+            <a href="https://twitter.com/johndoe" className="mr-4 text-blue-500 hover:underline">Twitter</a>
+            <a href="https://github.com/johndoe" className="text-blue-500 hover:underline">GitHub</a>
           </div>
         </div>
-      </section>
-      <section className="mb-8">
-        <h2 className="text-xl font-bold mb-2 border-b-2 pb-2">Education</h2>
-        <ul className="list-disc list-inside">
-        <div>
-          <div className="mb-4">
-            <h3 className="text-lg font-bold">Company A</h3>
-            <p><em>Address A</em></p>
-            <p><em>Position A | Duration A</em></p>
-            <ul className="list-disc list-inside">
-              <li>Responsibility A1</li>
-              <li>Responsibility A2</li>
-            </ul>
-          </div>
-          <div className="mb-4">
-            <h3 className="text-lg font-bold">Company B</h3>
-            <p><em>Address B</em></p>
-            <p><em>Position B | Duration B</em></p>
-            <ul className="list-disc list-inside">
-              <li>Responsibility B1</li>
-              <li>Responsibility B2</li>
-            </ul>
+        {/* About Me */}
+        <div className="border-t border-gray-200">
+          <div className="bg-white px-4 py-5 sm:px-6">
+            <dt className="text-lg font-medium text-gray-500 mb-2">About Me</dt>
+            <div className="flex flex-wrap">
+              Adaptable Management and software development major aiming to leverage proven communication, creative thinking and management skills to successfully fill the software developer at your xompany. 
+              I can be relied upon to help your company achieve its goal.
+            </div>
           </div>
         </div>
-          <li>University B - Degree B</li>
-        </ul>
-      </section>
-      <section className="mb-8">
-        <h2 className="text-xl font-bold mb-2 border-b-2 pb-2">Skills</h2>
-        <ul className="list-disc list-inside">
-          <li>Skill A</li>
-          <li>Skill B</li>
-        </ul>
-      </section>
-      <footer>
-        <a href="#" className="text-blue-500 hover:underline mr-4">LinkedIn</a>
-        <a href="#" className="text-blue-500 hover:underline mr-4">GitHub</a>
-        <a href="#" className="text-blue-500 hover:underline">Twitter</a>
-      </footer>
+        {/* Skills */}
+        <div className="border-t border-gray-200">
+          <div className="bg-white px-4 py-5 sm:px-6">
+            <dt className="text-lg font-medium text-gray-500 mb-2">Skills</dt>
+            <div className="flex flex-wrap">
+              {skills.map((skill, index) => (
+                <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{skill}</span>
+              ))}
+            </div>
+          </div>
+        </div>
+        {/* Experience */}
+        <div className="border-t border-gray-200">
+          <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-lg font-medium text-gray-500">Experience</dt>
+            <dd className="mt-1 text-lg text-gray-900 sm:col-span-2">
+              <div>
+                <div className="text-xl font-bold">Synergy Info Systems</div>
+                <div className="text-sm italic text-gray-600">Baluwatar, Kathmandu</div>
+              </div>
+              <div className="mt-2">
+                <span className="font-bold italic">Software Developer</span>, <span className="text-sm italic">Apr 2022 - Present</span>
+              </div>
+              <ul className="list-disc list-inside text-sm">
+              <li>Maintain and update existing application</li>
+              <li>Implementation of new logics</li>
+              <li>Lead and train interns and new joinee</li>
+              <li>Debug and remove existing bugs and issues</li>
+              <li>Modify the application as per request</li>
+              <li>Quickly adapt with new technologies</li>
+                <li>Implementing agile development priciples</li>
+              </ul>
+            </dd>
+            <dt className="text-lg font-medium text-gray-500"></dt>
+            <dd className="mt-1 text-lg text-gray-900 sm:col-span-2">
+              <div>
+                <div className="text-xl font-bold">Synergy Info Systems</div>
+                <div className="text-sm italic text-gray-600">Baluwatar, Kathmandu</div>
+              </div>
+              <div className="mt-2">
+                <span className="font-bold italic">Software Developer Intern</span>, <span className="text-sm italic">March 2022 - Apr 2022</span>
+              </div>
+              <ul className="list-disc list-inside text-sm ">
+                <li>Understanding of programming basics</li>
+                <li>Understanding of professional work environment</li>
+                <li>Knowledge of different tools</li>
+                <li>Develop readable code</li>
+              </ul>
+            </dd>
+          </div>
+        </div>
+        {/* Education */}
+        <div className="border-t border-gray-200">
+          <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt className="text-lg font-medium text-gray-500">Education</dt>
+            <dd className="mt-1 text-lg text-gray-900 sm:col-span-2">
+              <div>
+                <div className="font-bold">Masters of Information Technology</div>
+                <span>Amrit Science Campus</span>, <span className="text-sm italic">Lainchaur, Kathmandu</span>
+                <div className="text-sm italic text-gray-600">04/2023 - Present</div>
+              </div>
+            </dd>
+            <dt className="text-lg font-medium text-gray-500"></dt>
+            <dd className="mt-1 text-lg text-gray-900 sm:col-span-2">
+            <div>
+                <div className="font-bold">Bachelor of Information Management</div>
+                <span>Nepal Commerce Campus</span>, <span className="text-sm italic">Minbhawan, Kathmandu</span>
+                <div className="text-sm italic text-gray-600">10/2017 - 07/2022</div>
+              </div>
+            </dd>
+            <dt className="text-lg font-medium text-gray-500"></dt>
+            <dd className="mt-1 text-lg text-gray-900 sm:col-span-2">
+            <div>
+                <div className="font-bold">Higher Secondary School</div>
+                <span>GoldenGate Int'l School</span>, <span className="text-sm italic">Old Baneshwor, Kathmandu</span>
+                <div className="text-sm italic text-gray-600">08/2015 - 08/2017</div>
+              </div>
+            </dd>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default ResumeSite;
+export default Resume;
